dbfe4be32a8bb54e23e8dcb0e183254bc36c4d0b
abmali05 <bestofhonest@gmail.com>
2022-04-29 17:40:52 +0600

    different pages added

---
+++ src/Pages/Home/Product/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <div>
+            <h2>This product</h2>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/Pages/Login/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+    return (
+        <div>
+            <h2>This is auth</h2>
+        </div>
+    );
+};
+
+export default Auth;
