773fc90a02d9170b45b5537b5721b4eef40ed179
abmali05 <bestofhonest@gmail.com>
2022-05-06 17:32:56 +0600

    top brands finish

--- src/Pages/TopBrands/TopBrands.js
+++ src/Pages/TopBrands/TopBrands.js
@@ -1,36 +1,56 @@
 import React from 'react';
-import img1 from './samsung-logo.png';
-import img2 from './apple-logo.png';
-import img3 from './vivo-logo.png';
+import img1 from './samsung-logo1.png';
+import img2 from './xiaomi-logo5.png';
+// import img2 from './xiaomi-logo5.png';
+import img3 from './vivo-logo3.png';
 
 const TopBrands = () => {
     return (
-        <div>
-            <div class="d-flex align-items-center">
-                <div class="flex-shrink-0">
+
+        <div class="container mt-5">
+            <h2 className='text-center mb-3'>Top Brands</h2>
+
+            <div class="row text-center">
+                <div class="col-md-4 mb-2">
                     <img src={img1} alt="..." style={{ width: "250px", height: "auto" }} className='img-responsive' />
                 </div>
-                <div class="flex-grow-1 ms-3">
-                    This is some content from a media component. You can replace this with any content and adjust it as needed.
-                </div>
-            </div>
-            <div class="d-flex align-items-center">
-                <div class="flex-shrink-0">
-                    <img src={img2} alt="..." style={{ width: "100px", height: "auto" }} className='img-responsive' />
-                </div>
-                <div class="flex-grow-1 ms-3">
-                    This is some content from a media component. You can replace this with any content and adjust it as needed.
-                </div>
-            </div>
-            <div class="d-flex align-items-center">
-                <div class="flex-shrink-0">
+                <div class="col-md-4 mb-4">
                     <img src={img3} alt="..." style={{ width: "250px", height: "auto" }} className='img-responsive' />
                 </div>
-                <div class="flex-grow-1 ms-3">
-                    This is some content from a media component. You can replace this with any content and adjust it as needed.
+                <div class="col-md-4 mb-2">
+                    <img src={img2} alt="..." style={{ width: "240px", height: "auto" }} className='img-responsive' />
                 </div>
+                {/* <div class="col-md-3">
+                    <img src={img3} alt="..." style={{ width: "250px", height: "auto" }} className='img-responsive' />
+                </div> */}
             </div>
         </div>
+        // <div>
+        //     <div class="d-flex align-items-center">
+        //         <div class="flex-shrink-0">
+        //             <img src={img1} alt="..." style={{ width: "250px", height: "auto" }} className='img-responsive' />
+        //         </div>
+        //         <div class="flex-grow-1 ms-3">
+        //             This is some content from a media component. You can replace this with any content and adjust it as needed.
+        //         </div>
+        //     </div>
+        //     <div class="d-flex align-items-center">
+        //         <div class="flex-shrink-0">
+        //             <img src={img2} alt="..." style={{ width: "100px", height: "auto" }} className='img-responsive' />
+        //         </div>
+        //         <div class="flex-grow-1 ms-3">
+        //             This is some content from a media component. You can replace this with any content and adjust it as needed.
+        //         </div>
+        //     </div>
+        //     <div class="d-flex align-items-center">
+        //         <div class="flex-shrink-0">
+        //             <img src={img3} alt="..." style={{ width: "250px", height: "auto" }} className='img-responsive' />
+        //         </div>
+        //         <div class="flex-grow-1 ms-3">
+        //             This is some content from a media component. You can replace this with any content and adjust it as needed.
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
